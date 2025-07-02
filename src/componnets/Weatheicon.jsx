eda6ee22b93ee40474a1af17bd32f2d912b2a3e9
rahulgope45 <rahuleile456@gmail.com>
2025-07-02 11:49:41 +0530

    Icon added

--- src/componnets/Weatheicon.jsx
+++ src/componnets/Weatheicon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Weatheicon({iconnumber,summary}) {
   return (
     
-         <img src={`dist/weather_icons/set03/big/${iconnumber}.png`}
+         <img src={`weather_icons/set03/big/${iconnumber}.png`}
             alt={summary}
             draggable={false}/>
     
